fee3b24638cb0c1f8da83cfa778b7dc3972af6f8
Berat Ünlü <90249269+beratunlu@users.noreply.github.com>
2022-04-11 01:49:04 +0300

    Create sifre-uretme.py

---
+++ sifre-uretme.py
@@ -0,0 +1,27 @@
+import random 
+    
+
+import string 
+
+
+  
+
+
+def pass_uzunlugu(size): 
+        
+ 
+    
+    generate_pass = "{}".join(random.choice( string.ascii_uppercase + string.ascii_lowercase + string.digits + string.punctuation)   
+    for n in range(size)) 
+                            
+    return generate_pass 
+
+
+
+
+    
+# Driver Code 
+size = int(input("Lutfen olmasını istediginiz sifre uzunlugunu belirtiniz: ")) 
+password = pass_uzunlugu(size)
+
+print(password) 
